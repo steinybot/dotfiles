5ce1905e8e258c9e0a9cbcc9ecdcd6f6d57ea833
Jason Pickens <jasonpickensnz@gmail.com>
2022-02-01 07:41:48 +1300

    Update nix configuration

--- home/.nixpkgs/darwin-configuration.nix
+++ home/.nixpkgs/darwin-configuration.nix
@@ -3,17 +3,18 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [ pkgs.vim
-    ];
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
@@ -23,5 +24,4 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  # TODO: Delete me.
 }
