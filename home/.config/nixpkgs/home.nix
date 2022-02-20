75a2116c02731031c685d5507205bd346e00431f
Jason Pickens <jasonpickensnz@gmail.com>
2022-02-21 12:30:13 +1300

    Make gpg-agent.conf inline

--- home/.config/nixpkgs/home.nix
+++ home/.config/nixpkgs/home.nix
@@ -1,40 +1,54 @@
 { config, pkgs, ... }:
 
 let
+  username = "jason";
+  homeDirectory = "/Users/${username}";
+  
   # The path to this repository once it has been checked out.
   dotFilesRepo = fetchGit {
     url = "https://github.com/steinybot/dotfiles.git";
     ref = "main";
   };
-  homeFilesDirectory = "${dotFilesRepo}/home";
+  repoHomeFilesDirectory = "${dotFilesRepo}/home";
 
   # Add these manually so that we can specify custom onChange etc.
   managedHomeFiles = {
     # This may update itself so we might need to run again.
     ".config/nixpkgs/home.nix" = {
-      source = "${homeFilesDirectory}/.config/nixpkgs/home.nix";
+      source = "${repoHomeFilesDirectory}/.config/nixpkgs/home.nix";
       onChange = "home-manager --option tarball-ttl 0 switch";
     };
 
     # Run rebuild when the configuration changes.
     ".nixpkgs/darwin-configuration.nix" = {
-      source = "${homeFilesDirectory}/.nixpkgs/darwin-configuration.nix";
+      source = "${repoHomeFilesDirectory}/.nixpkgs/darwin-configuration.nix";
       onChange = "darwin-rebuild switch";
     };
 
     # Update the channels when they change.
     # FIXME: This onChange needs to run first.
     ".nix-channels" = {
-      source = "${homeFilesDirectory}/.nix-channels";
+      source = "${repoHomeFilesDirectory}/.nix-channels";
       onChange = "nix-channel --update";
+    };
+    
+    ".gnupg/gpg-agent.conf" = {
+      text = ''
+        enable-ssh-support
+        pinentry-program ${homeDirectory}/.nix-profile/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
+        default-cache-ttl 600
+        max-cache-ttl 7200
+        default-cache-ttl-ssh 600
+        max-cache-ttl-ssh 7200
+      '';
     };
   };
   managedHomeFileNames = builtins.attrNames managedHomeFiles;
 
   # Add all the rest of the files in home automatically.
-  unmanagedHomeFileNames = builtins.removeAttrs (builtins.readDir homeFilesDirectory) managedHomeFileNames;
+  unmanagedHomeFileNames = builtins.removeAttrs (builtins.readDir repoHomeFilesDirectory) managedHomeFileNames;
   unmanagedHomeFiles = builtins.mapAttrs (name: value: {
-      source = "${homeFilesDirectory}/${name}";
+      source = "${repoHomeFilesDirectory}/${name}";
       # This has to be recursive otherwise we get an error saying:
       # Error installing file '...' outside $HOME
       # When using something like programs.git which will try and write
@@ -63,8 +77,8 @@ in
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "jason";
-  home.homeDirectory = "/Users/jason";
+  home.username = username;
+  home.homeDirectory = homeDirectory;
 
   # Link home files.
   home.file = managedHomeFiles // unmanagedHomeFiles;
