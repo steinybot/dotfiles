6cfd3ba1d2ff84d7ee788ff77d39730818a99421
Jason Pickens <jasonpickensnz@gmail.com>
2022-02-23 06:21:26 +1300

    Enable ZSH syntax highlighting

--- home/.nixpkgs/darwin-configuration.nix
+++ home/.nixpkgs/darwin-configuration.nix
@@ -41,7 +41,10 @@ in
   };
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh = {
+    enable = true;  # default shell on catalina
+    enableSyntaxHighlighting = true;
+  };
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
