35665f670d1dbfdfb2b923b9051b99943b5000c0
Jason Pickens <jasonpickensnz@gmail.com>
2022-02-24 13:39:05 +1300

    Add configuration

--- home/.nixpkgs/darwin-configuration.nix
+++ home/.nixpkgs/darwin-configuration.nix
@@ -14,6 +14,7 @@ let
   customPkgs = import customPkgsRepo {};
   customIntelPkgs = import customPkgsRepo { system = "x86_64-darwin"; };
   customPackages = with customPkgs; [
+    # FIXME: This is not true.
     # GUI applications have to be installed with Nix Darwin and not Home Manager otherwise they
     # do not work with Spotlight etc. See https://github.com/nix-community/home-manager/issues/1341.
     iterm2
@@ -23,41 +24,96 @@ let
   ];
 in
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = packages ++ customPackages;
+  environment = {
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    systemPackages = packages ++ customPackages;
 
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
-
-  nix.extraOptions = ''
-    # With Rosetta 2 we can run Intell apps on Apple M1.
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
-
-  nix.package = pkgs.nix;
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+    #variables = {
+    #  # Set NIX_PATH explicitly to work around https://github.com/NixOS/nixpkgs/issues/149791.
+    #  # The path is the same as the default when NIX_PATH is not set (https://github.com/NixOS/nix/blob/master/src/libexpr/eval.cc).
+    #  NIX_PATH = "\${NIX_PATH:-\${HOME}/.nix-defexpr/channels:nixpkgs=${nixStateDir}/profiles/per-user/root/channels/nixpkgs:${nixStateDir}/profiles/per-user/root/channels}";
+    #};
   };
 
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh = {
-    enable = true;  # default shell on catalina
-    enableSyntaxHighlighting = true;
+  networking = {
+    computerName = "Goodness Gracious";
+    hostName = "goodness";
   };
-  # programs.fish.enable = true;
+
+  nix = {
+    extraOptions = ''
+      # With Rosetta 2 we can run Intell apps on Apple M1.
+      extra-platforms = x86_64-darwin aarch64-darwin
+    '';
+
+    nixPath = [
+      {
+        darwin-config = "\${HOME}/.nixpkgs/darwin-configuration.nix";
+        nixpkgs = "/nix/var/nix/profiles/per-user/root/channels/nixpkgs";
+      }
+      "/nix/var/nix/profiles/per-user/root/channels"
+      "\${HOME}/.nix-defexpr/channels"
+    ];
+
+    package = pkgs.nix;
+  };
+
+  programs = {
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    zsh = {
+      enable = true;
+      enableSyntaxHighlighting = true;
+    };
+  };
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
-  system.defaults = {
-    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+  system = {
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        ApplePressAndHoldEnabled = true;
+        AppleShowAllExtensions = true;
+      };
+
+      SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+
+      dock = {
+        mru-spaces = false;
+        show-recents = false;
+      };
+
+      finder = {
+        FXEnableExtensionChangeWarning = false;
+        QuitMenuItem = true;
+        _FXShowPosixPathInTitle = true;
+      };
+
+      loginwindow = {
+        GuestEnabled = false;
+      };
+    };
+
+    keyboard.remapCapsLockToEscape = true;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 4;
   };
 
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  time.timeZone = "Pacific/Auckland";
+
+  users.users.jason = {
+    createHome = true;
+    description = "Jason Pickens";
+    home = "/Users/jason";
+    name = "Jason Pickens";
+    shell = pkgs.oh-my-zsh;
+  };
 }
--- home/.zshrc
+++ home/.zshrc
@@ -1,9 +1,2 @@
 alias nix-bootstrap="sh <(curl -L https://raw.githubusercontent.com/steinybot/bootstrap/main/bootstrap.sh)"
 alias home-update="home-manager switch --option tarball-ttl 0"
-
-# TODO: Put exports somewhere else.
-
-# Set NIX_PATH explicitly to work around https://github.com/NixOS/nixpkgs/issues/149791.
-# The path is the same as the default when NIX_PATH is not set (https://github.com/NixOS/nix/blob/master/src/libexpr/eval.cc).
-NIX_STATE_DIR="/nix/var/nix"
-export NIX_PATH="${NIX_PATH:-${HOME}/.nix-defexpr/channels:nixpkgs=${NIX_STATE_DIR}/profiles/per-user/root/channels/nixpkgs:${NIX_STATE_DIR}/profiles/per-user/root/channels}"
